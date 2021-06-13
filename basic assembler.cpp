51e57b629a24a4d77e36fda0357bcd9e9ba3d35f
Forcer0625 <61505055+Forcer0625@users.noreply.github.com>
2021-06-13 17:15:20 +0800

    PASS2 end half

--- basic assembler.cpp
+++ basic assembler.cpp
@@ -9,10 +9,12 @@ using namespace std;
 #define OUTPUT1 "PASS1_location_counter.txt"
 #define OUTPUT2 "PASS1_symbol_table.txt"
 #define OUTPUT3 "PASS2_object_code.txt"
+#define OUTPUT4 "PASS2_final_object_program.txt"
 #define SOURCE_FORMAT "%s\t%s\t%X\n" //failed
 #define OPCODE_FORMAT "%s %c%c\n"
 #define SYMBOL_FORMAT "%s\t%X\n"
 #define C_EOF "454F46"
+
 
 unsigned int pow(unsigned int base,int power)
 {
@@ -20,6 +22,52 @@ unsigned int pow(unsigned int base,int power)
         return (base * pow(base, power - 1));
     else
         return 1;
+}
+char* HexToStr(unsigned int input)
+{
+    char* re=(char*)malloc(sizeof(char)*5); //4+1('\0')
+    //char re[5];
+    re[0]=re[1]=re[2]=re[3]=re[4]='\0';     //initialize
+    re[0]=  input/pow(16,3);
+            input-=(re[0]*pow(16,3));
+    re[1]=  input/pow(16,2);
+            input-=(re[1]*pow(16,2));
+    re[2]=  input/16;
+            input-=(re[2]*16);
+    re[3]=  input;
+
+    int ind=3;
+    while(ind>=0)
+    {
+        char c=re[ind];
+        switch (c)
+        {
+            case 10:
+                re[ind]='A';
+                break;
+            case 11:
+                re[ind]='B';
+                break;
+            case 12:
+                re[ind]='C';
+                break;
+            case 13:
+                re[ind]='D';
+                break;
+            case 14:
+                re[ind]='E';
+                break;
+            case 15:
+                re[ind]='F';
+                break;
+            default:
+                re[ind]+='0';
+                break;
+        }
+        ind--;
+    }
+
+    return re;
 }
 unsigned int StrToDec(const char* input)
 {
@@ -34,7 +82,7 @@ unsigned int StrToDec(const char* input)
     }
     return re;
 }
-unsigned int StrToInt(const char* input)
+unsigned int StrToHex(const char* input)
 {
     unsigned int re=0;
     int len = strlen(input);
@@ -94,7 +142,7 @@ class OperationCode
                 operation[i].codestr[1]=num2;
                 operation[i].codestr[2]='\0';
                 operation[i].code=                          //store operation code
-                StrToInt(operation[i].codestr); 
+                StrToHex(operation[i].codestr); 
 
             }
             
@@ -145,6 +193,18 @@ class SymbolTable
         {
             head=NULL;
         }
+        void build(const char* _SymbolTableFileName)
+        {
+            head=NULL;
+
+            FILE* input=fopen(_SymbolTableFileName,"r");
+            if(!input) {printf("%s file not found.\n",_SymbolTableFileName);system("PAUSE");exit(1);}
+            char tempstr[16]={'\0'}; //Symbol name
+            unsigned int address;    //Symbol location
+
+            while(fscanf(input,SYMBOL_FORMAT,tempstr,&address)!=EOF)
+                addSymbol(tempstr,address);
+        }
         Symbol* query(const char* target)
         {
             for(Symbol* temp=head;temp;temp=temp->next)
@@ -184,8 +244,141 @@ class SymbolTable
         }
 
 };
-//This Function will create program location and symbol table.
-void Pass1(const char* _SourceFileName)
+class HeaderRecord
+{
+    /*
+     * Header record:
+     * Col.1        H
+     * Col.2-7      Program name
+     * Col.8-13     Starting address (hex)
+     * Col.14-19    Length of object program in bytes (hex)
+     */
+    private:
+        char head;
+        char programName    [7]={' '};
+        char startingAddress[7]={'0'};
+        char lengthInByte   [7]={'0'};
+    public :
+    void build(const char* _programName,const char* _startAddress,const char* _length)
+    {
+        head='H';
+        for(int i=0;i<6;i++)
+        {
+            programName     [i]=' ';
+            startingAddress [i]='0';
+            lengthInByte    [i]='0';
+        }
+        programName     [6]='\0';
+        startingAddress [6]='\0';
+        lengthInByte    [6]='\0';
+        recordName  (_programName);
+        recordStart (_startAddress);
+        recordLength(_length);
+    }
+    void recordName(const char* _programName)
+    {
+        int len=strlen(_programName);
+        for(int i=0;i<len;i++)
+            programName[i]=_programName[i];
+    }
+    void recordStart(const char* _startAddress)
+    {
+        int len=strlen(_startAddress);
+        int ind=5;
+        for(int i=len-1;i>=0;i--)
+            startingAddress[ind--]=_startAddress[i];
+    }
+    void recordLength(const char* _length)
+    {
+        int len=strlen(_length);
+        int ind=5;
+        for(int i=len-1;i>=0;i--)
+            lengthInByte[ind--]=_length[i];
+    }
+    void write(FILE* output)
+    {
+        fprintf(output,"%c%s%s%s\n",head,programName,startingAddress,lengthInByte);
+    }
+    void del()
+    {
+        free(this);
+    }
+};
+class TextRecord
+{
+    /*
+     * Text record:
+     * Col.1       T
+     * Col.2-7     Starting address in this record (hex)
+     * Col.8-9     Length of object code in this record in bytes
+     * Col.10-69   Object code (hex) (2 columns per byte)
+     */
+    private:
+        char head;
+        char startingAddress[7];
+        char length[3];
+        char objectCode[61];
+    public :
+    void initialize()
+    {
+        head='T';
+        for(int i=0;i<6;i++)
+            startingAddress [i]='0';
+            startingAddress [6]='\0';
+        for(int i=0;i<61;i++)
+            objectCode[i]='\0';
+        length[0]=length[1]='0';
+        length[2]='\0';
+    }
+    void recordStart(const char* _startAddress)
+    {
+        int len=strlen(_startAddress);
+        int ind=5;
+        for(int i=len-1;i>=0;i--)
+            startingAddress[ind--]=_startAddress[i];
+    }
+    bool isFull()
+    {
+        
+    }
+};
+class EndRecord
+{
+    /*
+     * End record:
+     * Col.1    E
+     * Col.2~7  Address of first executable instruction (hex)
+     */
+    private:
+        char head;
+        char EndAddress[7];//Address of first executable instruction
+    public :
+    void build(const char* _EndAddress)
+    {
+        head='E';
+        for(int i=0;i<6;i++)
+            EndAddress[i]='0';
+        EndAddress[6]='\0';
+        recordAddress(_EndAddress);
+    }
+    void recordAddress(const char* _EndAddress)
+    {
+        int len=strlen(_EndAddress);
+        int ind=5;
+        for(int i=len-1;i>=0;i--)
+            EndAddress[ind--]=_EndAddress[i];
+    }
+    void write(FILE* output)
+    {
+        fprintf(output,"%c%s",head,EndAddress);
+    }
+    void del()
+    {
+        free(this);
+    }
+};
+//This Function will create program location and symbol table, then return program length.
+unsigned int Pass1(const char* _SourceFileName)
 {
     //Create Operation Table and Symbol Table
     OperationCode* OpTable=(OperationCode*)malloc(sizeof(OperationCode));
@@ -214,7 +407,7 @@ void Pass1(const char* _SourceFileName)
         {
             if(strcmp(tempstr[1],"START") == 0) //Opcode=START
             {
-                START_Address = LOCCTR = StrToInt(tempstr[2]);
+                START_Address = LOCCTR = StrToHex(tempstr[2]);
                 //initialize LOCCTR to START address
                 fprintf(output1,"%X\t%s\t%s\t%s\n"
                 ,LOCCTR,tempstr[0],tempstr[1],tempstr[2]);
@@ -247,28 +440,35 @@ void Pass1(const char* _SourceFileName)
                 Operation* found=OpTable->query(tempstr[1]);
                 if(found)
                 {
+                    //instruction=>LOCCTR add 3*instruction length (btye)
                     unsigned int instruction_length=strlen(found->codestr)>>1;//two hex bit for one byte
                     LOCCTR += 3*instruction_length;
                 }
                 else if(!strcmp(tempstr[1],"WORD"))
+                    //WORD=>LOCCTR add 3 byte
                     LOCCTR += 3;
                 else if(!strcmp(tempstr[1],"RESW"))
+                    //RESW=>LOCCTR add 3*#[OPERAND] (byte)
                     LOCCTR += 3*StrToDec(tempstr[2]);
                 else if(!strcmp(tempstr[1],"RESB"))
+                    //RESB=>LOCCTR add #[OPERAND] (byte)
                     LOCCTR += StrToDec(tempstr[2]);
                 else if(!strcmp(tempstr[1],"BYTE"))
                 {
+                    //BYTE=>convert constant length in byte
                     if(!strcmp("C\'EOF\'",tempstr[2]))
                         strcpy(tempstr[2],C_EOF);
                     else
                     {
+                        //find the constant in ' '
                         char constant[5] = {'\0'};
                         int i=0,len=strlen(tempstr[2]);
                         for(;i<len;i++) if(tempstr[2][i]=='\'') break;
                         for(int j=0;tempstr[2][++i]!='\'';j++) constant[j]=tempstr[2][i];
                         strcpy(tempstr[2],constant);
                     }
-                    unsigned int constant_length=strlen(tempstr[2])>>1;//2 hex bit for 1 byte
+                    //2 hex bit for 1 byte
+                    unsigned int constant_length=strlen(tempstr[2])>>1;
                     LOCCTR += constant_length;
                 }
                 else
@@ -295,11 +495,159 @@ void Pass1(const char* _SourceFileName)
     }while(strcmp(tempstr[1],"END") != 0);
     fprintf(output1,"\t\t%s\t%s","END",SymTable->findLast()->symbolname);
 
+    //close file
+    fclose(input);
+    fclose(output1);
+    fclose(output2);
+
     //delete Operation Table and Symbol Table
     OpTable ->del();
     SymTable->del();
-}
 
+    return LOCCTR-START_Address;
+}
+//This Function will create each statement's objetcode.
+void Pass2(const char* _LocationFileName,const char* _SymbolTableFileName,unsigned int _ProgramLength)
+{
+    //Create Operation Table and Symbol Table
+    OperationCode* OpTable=(OperationCode*)malloc(sizeof(OperationCode));
+    SymbolTable* SymTable=(SymbolTable*)malloc(sizeof(SymbolTable));
+    OpTable ->build(OPCODE);
+    SymTable->build(_SymbolTableFileName);
+
+    
+    FILE* input  =fopen(_LocationFileName,"r");
+    if(!input) {printf("file %s not found.\n",_LocationFileName);system("PAUSE");exit(1);}
+    FILE* output3=fopen(OUTPUT3,"w");
+    FILE* output4=fopen(OUTPUT4,"w");
+
+    char tempstr[4][16]={"","",""}; //to process via
+    /*
+     *  tempstr[0] for Location(processed via pass1)
+     *  tempstr[1] for Symbol
+     *  tempstr[2] for Opcode(instruction)
+     *  tempstr[3] for Operand
+     */
+    char c; int i=0,j=0;
+    do
+    {
+        c = fgetc(input);
+        if(c == '\n')
+        {
+            if(!strcmp(tempstr[2],"START"))
+            {
+                //Write lisitng line
+                fprintf(output3,"%s\t%s\t%s\t%s\n",
+                    tempstr[0],tempstr[1],tempstr[2],tempstr[3]);
+                //Write Header recoed to final object file
+                HeaderRecord* rec=(HeaderRecord*)malloc(sizeof(HeaderRecord));
+                char*   temp=HexToStr(_ProgramLength);
+                rec->build(tempstr[1],tempstr[3],temp);
+                rec->write(output4);
+                rec->del();
+                free(temp);
+            }
+            else
+            {
+                fprintf(output3,"%s\t%s\t%s\t%s",
+                    tempstr[0],tempstr[1],tempstr[2],tempstr[3]);
+                char objcode[7]={'\0'};
+                Operation* found=OpTable->query(tempstr[2]);
+                if(found)
+                {
+                    strcpy(objcode,found->codestr);
+                    //if there is a symbol in OPERAMD field
+                    if(tempstr[3][0]!='\0')
+                    {
+                        Symbol* symbol;
+
+                        //remove ,X to get real symbol
+                        for(int i=0;i<strlen(tempstr[3]);i++)
+                        if(tempstr[3][i]==',')  {tempstr[3][i]='\0';break;}
+
+                        if(symbol=SymTable->query(tempstr[3]))
+                        {
+                            //store symbol value as operand address
+                            char* trans=HexToStr(symbol->address);
+                            strcpy(tempstr[3],trans);
+                            free(trans);
+                            strcat(objcode,tempstr[3]);
+                        }
+                        else
+                        {
+                            //Error:undefined symbol
+                            printf("Error:undefined symbol %s\t",tempstr[3]);
+                            system("PAUSE");
+                            exit(1);
+                        }
+                    }
+                    else
+                    {
+                        //store 0 as operand
+                        strcat(objcode,"0000");
+                    }
+                    
+                }
+                else if(!strcmp("BYTE",tempstr[2]))
+                {
+                    //covert constant to object code
+                    if(!strcmp("C\'EOF\'",tempstr[3]))
+                        strcpy(objcode,C_EOF);
+                    else
+                    {
+                        //find the constant in ' '
+                        int i=0,len=strlen(tempstr[3]);
+                        for(;i<len;i++) if(tempstr[3][i]=='\'') break;
+                        for(int j=0;tempstr[3][++i]!='\'';j++) objcode[j]=tempstr[3][i];
+                    }
+                }
+                else if(!strcmp("WORD",tempstr[2]))
+                {
+                    //opreation refill 0 to high hex bit
+                    objcode[0]='0';
+                    objcode[1]='0';
+                    //translate immediate value to opreand
+                    char *trans=HexToStr(StrToDec(tempstr[3]));
+                    strcat(objcode,trans);
+                    free(trans);
+                }
+                
+                fprintf(output3,"\t%s\n",objcode);
+
+
+            }
+            
+
+            //reset temp string
+            for(j= 4;j>0;j--)
+            for(i=16;i>0;i--)
+            tempstr[j-1][i-1]='\0';
+        }
+        else
+        {
+            if(c == '\t')   {j++;continue;}
+            sprintf(tempstr[j],"%s%c",tempstr[j],c);
+        }
+    }while(strcmp(tempstr[2],"END") != 0);
+    fprintf(output3,"\t\t%s\t%s","END",SymTable->findLast()->symbolname);
+
+    //
+    char* endadd=HexToStr(SymTable->findLast()->address);
+    EndRecord* end=(EndRecord*)malloc(sizeof(EndRecord));
+    end->build(endadd);
+    end->write(output4);
+    free(endadd);
+
+    //close files
+    fclose(input);
+    fclose(output3);
+    fclose(output4);
+
+    //delete Operation Table and Symbol Table
+    OpTable ->del();
+    SymTable->del();
+
+}
 int main()
 {
     //OperationCode* OpTable=(OperationCode*)malloc(sizeof(OperationCode));
@@ -310,8 +658,12 @@ int main()
     
     //unsigned int t=StrToDec("");
     //printf("%u\n",t);
-    
-    Pass1(SOURCE);
+    //char* temp=HexToStr(0x460);
+    //printf("%s\n",temp);
+
+    unsigned int _ProgramLength=Pass1(SOURCE);
+    //printf("%X",_ProgramLength);
+    Pass2(OUTPUT1,OUTPUT2,_ProgramLength);
 
     system("PAUSE");
     return 0;
